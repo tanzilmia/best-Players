78f3d433e34a95e694753c57ff67d0fc592120bf
tanzilmia <ziltan590@gmail.com>
2022-08-22 10:47:29 +0600

    calculation section

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-// get all global veriable 
-let perPlayerCost = document.getElementById('perPlayer_cost');
+
+// global veribal
+
 let Orderlist = document.getElementById('list');
 let perPlayecost = document.getElementById('perPlayer_cost');
 let playerExpence = document.getElementById('player_expence');
-let managerCost = document.getElementById('manager_cost');
-let CoachCost = document.getElementById('Coach_cost');
 let totalCost = document.getElementById('total_cost');
 // buttons select
 let Calculetbtn = document.getElementById('player_calculet');
 let totalCalculetbtn = document.getElementById('total_calculet');
-
-
-
-
-
 // a functio for btn click
 function btnClick(btn){
     let players = btn.parentNode.children[0].innerText
     let li = document.createElement('li')
     Orderlist.appendChild(li)
-    let ListLength = Orderlist.childNodes.length -3
+    let ListLength = Orderlist.children.length
     if(ListLength >=6){
-        li.style.display ='none'
+        li.remove()
         alert('you can select just 5 players')
     }else{
         li.innerText = players
@@ -29,7 +23,33 @@ function btnClick(btn){
     }
 }
 
+// string to value convertion 
 
+function convertStrToValue(elemnt){
+    let inputValue = document.getElementById(elemnt)
+    let playerValue = parseInt(inputValue.value)
+    return playerValue
+}
+
+
+
+// calculation for palyers -----
+Calculetbtn.addEventListener('click',function(){
+    let TotalPlayers = Orderlist.children.length
+    let playerCost = convertStrToValue('perPlayer_cost')
+    let cost = TotalPlayers * playerCost
+    // show value on the main part
+    playerExpence.innerText = cost
+})
+// final calculation ------
+totalCalculetbtn.addEventListener('click',function(){
+    let playerExpen = parseInt(playerExpence.innerText)
+    let managerCost = convertStrToValue('manager_cost')
+    let coachCost = convertStrToValue('Coach_cost')
+    let total = playerExpen + managerCost + coachCost
+    // shwo total 
+    totalCost.innerText = total
+})
 
 
 

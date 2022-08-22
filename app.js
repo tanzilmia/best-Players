584850a8fbfb3280108b024492ae475ec3b8af31
tanzilmia <ziltan590@gmail.com>
2022-08-22 09:32:15 +0600

    select players

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // a functio for btn click
 
+function btnClick(btn){
+    let players = btn.parentNode.children[0].innerText
+    let Orderlist = document.getElementById('list')
+    let li = document.createElement('li')
+    Orderlist.appendChild(li)
+    let ListLength = Orderlist.childNodes.length -1
+    if(ListLength >=6){
+        li.style.display ='none'
+        alert('you can select just 5 players')
+    }else{
+        li.innerText = players
+        btn.setAttribute('disabled', '');
+    }
+}
 
 
 
